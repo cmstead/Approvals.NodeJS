831d154fd55935d2cbb121f6905e92176e741973
Chris Stead <cmstead@gmail.com>
2017-12-22 10:58:43 -0800

    Just removing edge altogether for my own purposes

--- install_dependencies.js
+++ install_dependencies.js
@@ -1,21 +1,21 @@
-var spawn = require('child_process').spawn;
-var isWin = !!process.platform.match(/^win/);
+// var spawn = require('child_process').spawn;
+// var isWin = !!process.platform.match(/^win/);
 
-console.log();
+// console.log();
 
-if (isWin) {
-  var cmd = spawn('npm.cmd', ['install', 'edge'], { cwd: __dirname });
+// if (isWin) {
+//   var cmd = spawn('npm.cmd', ['install', 'edge'], { cwd: __dirname });
 
-  cmd.stdout.on('data', function (data) {
-    console.log('' + data);
-  });
+//   cmd.stdout.on('data', function (data) {
+//     console.log('' + data);
+//   });
 
-  cmd.stderr.on('data', function (data) {
-    console.log('' + data);
-  });
+//   cmd.stderr.on('data', function (data) {
+//     console.log('' + data);
+//   });
 
-  cmd.on('close', function (code) {
-    console.log('npm install edge - completed with exit code - ' + code);
-  });
+//   cmd.on('close', function (code) {
+//     console.log('npm install edge - completed with exit code - ' + code);
+//   });
 
-}
+// }
